ab4b6a17b4f7ba3360934950c719482de4b1e642
Jerome Dixon <jerome.dixon90@gmail.com>
2020-07-12 13:58:59 -0400

    Update NLP_Processor.py

--- NLP_Processor.py
+++ NLP_Processor.py
@@ -1,192 +1,91 @@
-import pandas
+
 import pytextrank
 import spacy
-import networkx as nx
-import math
-import matplotlib.pyplot as plt
-import operator
+from math import sqrt
+
 
 nlp = spacy.load("en_core_web_sm")
 
-text = """Compatibility of systems of linear constraints over the set of natural numbers. Criteria of compatibility of
-a system of linear Diophantine equations, strict inequations, and nonstrict inequations are considered. Upper
-bounds for components of a minimal set of solutions and algorithms of construction of minimal generating sets
-of solutions for all types of systems are given. These criteria and the corresponding algorithms for
-constructing a minimal supporting set of solutions can be used in solving all the considered types systems
-and systems of mixed types."""
-
-doc = nlp(text)
-
-POS_KEPT = ["ADJ", "NOUN", "PROPN", "VERB"]
-
-
-def increment_edge(graph, node0, node1):
-    print("link {} {}".format(node0, node1))
-
-    if graph.has_edge(node0, node1):
-        graph[node0][node1]["weight"] += 1.0
-    else:
-        graph.add_edge(node0, node1, weight=1.0)
-
-
-def link_sentence(doc, lemma_graph, seen_lemma):
-    visited_tokens = []
-    visited_nodes = []
-
-    for sent in doc.sents:
-        print(">", sent.start, sent.end)
-
-    for i in range(sent.start, sent.end):
-        token = doc[i]
-
-        if token.pos_ in POS_KEPT:
-            key = (token.lemma_, token.pos_)
-
-            if key not in seen_lemma:
-                seen_lemma[key] = set([token.i])
-            else:
-                seen_lemma[key].add(token.i)
-
-            node_id = list(seen_lemma.keys()).index(key)
-
-            if not node_id in lemma_graph:
-                lemma_graph.add_node(node_id)
-
-            print("visit {} {}".format(visited_tokens, visited_nodes))
-            print("range {}".format(list(range(len(visited_tokens) - 1, -1, -1))))
-
-            for prev_token in range(len(visited_tokens) - 1, -1, -1):
-                print("prev_tok {} {}".format(prev_token, (token.i - visited_tokens[prev_token])))
-
-                if (token.i - visited_tokens[prev_token]) <= 3:
-                    lemma_graph.add_edge(lemma_graph, node_id, visited_nodes[prev_token])
-                else:
-                    break
-
-            print(" -- {} {} {} {} {} {}".format(token.i, token.text, token.lemma_, token.pos_, visited_tokens,
-                                                 visited_nodes))
-
-            visited_tokens.append(token.i)
-            visited_nodes.append(node_id)
-
-
-def draw_network_graph(doc):
-    lemma_graph = nx.Graph()
-    seen_lemma = {}
-
-    for sent in doc.sents:
-        link_sentence(doc, sent, lemma_graph, seen_lemma)
-
-    print(seen_lemma)
-
-    labels = {}
-    keys = list(seen_lemma.keys())
-
-    for i in range(len(seen_lemma)):
-        labels[i] = keys[i][0].lower()
-
-
-def view_graph(lemma_graph, labels):
-    fig = plt.figure(figsize=(9, 9))
-    pos = nx.spring_layout(lemma_graph)
-
-    nx.draw(lemma_graph, pos=pos, with_labels=False, font_weight="bold")
-    nx.draw_networkx_labels(lemma_graph, pos, labels)
-
-    plt.show(fig)
-
-
-def calculate_word_rank(lemma_graph, labels):
-    ranks = nx.pagerank(lemma_graph)
-    for node_id, rank in sorted(ranks.items(), key=lambda x: x[1], reverse=True):
-        print(node_id, rank, labels[node_id])
-
-
-def calculate_phrases(chunk, phrases, counts, seen_lemma, ranks, doc):
-    chunk_len = chunk.end - chunk.start + 1
-    sq_sum_rank = 0.0
-    non_lemma = 0
-    compound_key = set([])
-
-    for i in range(chunk.start, chunk.end):
-        token = doc[i]
-        key = (token.lemma_, token.pos_)
-
-        if key in seen_lemma:
-            node_id = list(seen_lemma.keys()).index(key)
-            rank = ranks[node_id]
-            sq_sum_rank += rank
-            compound_key.add(key)
-
-            print(" {} {} {} {}".format(token.lemma_, token.pos_, node_id, rank))
-        else:
-            non_lemma += 1
-
-    non_lemma_discount = chunk_len / (chunk_len + (2.0 * non_lemma) + 1.0)
-
-    # use root mean square (RMS) to normalize the contributions of all the tokens
-    phrase_rank = math.sqrt(sq_sum_rank / (chunk_len + non_lemma))
-    phrase_rank *= non_lemma_discount
-
-    # remove spurious punctuation
-    phrase = chunk.text.lower().replace("'", "")
-
-    # create a unique key for the the phrase based on its lemma components
-    compound_key = tuple(sorted(list(compound_key)))
-
-    if not compound_key in phrases:
-        phrases[compound_key] = set([(phrase, phrase_rank)])
-        counts[compound_key] = 1
-    else:
-        phrases[compound_key].add((phrase, phrase_rank))
-        counts[compound_key] += 1
-
-    print("{} {} {} {} {} {}".format(phrase_rank, chunk.text, chunk.start, chunk.end, chunk_len, counts[compound_key]))
-
-
-def collect_phrases(doc, labels, ranks):
-    phrases = {}
-    counts = {}
-    min_phrases = {}
-
-    for chunk in doc.noun_chunks:
-        collect_phrases(chunk, phrases, counts)
-
-    for compound_key, rank_tuples in phrases.items():
-        l = list(rank_tuples)
-        l.sort(key=operator.itemgetter(1), reverse=True)
-
-        phrase, rank = l[0]
-        count = counts[compound_key]
-
-        min_phrases[phrase] = (rank, count)
-
-    for phrase, (rank, count) in sorted(min_phrases.items(), key=lambda x: x[1][0], reverse=True):
-        print(phrase, count, rank)
-
-    for node_id, rank in sorted(ranks.items(), key=lambda x: x[1], reverse=True):
-        print(labels[node_id], rank)
-
-    for node_id, rank in sorted(ranks.items(), key=lambda x: x[1], reverse=True):
-        keywords = labels[node_id], rank
-        return keywords
-
+tr = pytextrank.TextRank()
+nlp.add_pipe(tr.PipelineComponent, name="textrank", last=True)
 
 def run_nlp(doc_to_process):
     doc = nlp(doc_to_process)
+    for p in doc._.phrases:
+        print("{:.4f} {:5d}  {}".format(p.rank, p.count, p.text))
+        print(p.chunks)
+    sent_bounds = [[s.start, s.end, set([])] for s in doc.sents]
+
+    limit_phrases = 4
+
+    phrase_id = 0
+    unit_vector = []
+
+    for p in doc._.phrases:
+        print(phrase_id, p.text, p.rank)
+
+        unit_vector.append(p.rank)
+
+        for chunk in p.chunks:
+            print(" ", chunk.start, chunk.end)
+
+            for sent_start, sent_end, sent_vector in sent_bounds:
+                if sent_start <= chunk.start <= sent_end:
+                    print(" ", sent_start, chunk.start, chunk.end, sent_end)
+                    sent_vector.add(phrase_id)
+                    break
+
+        phrase_id += 1
+
+        if phrase_id == limit_phrases:
+            break
+
     for sent in doc.sents:
-        print(">", sent.start, sent.end)
-    for chunk in doc.noun_chunks:
-        print(chunk.text)
-    for ent in doc.ents:
-        print(ent.text, ent.label_, ent.start, ent.end)
-    increment_edge()
-    link_sentence()
-    draw_network_graph()
-    view_graph()
-    calculate_word_rank()
-    calculate_phrases()
-    collect_phrases()
+        print(sent)
+
+    sum_ranks = sum(unit_vector)
+    unit_vector = [rank / sum_ranks for rank in unit_vector]
+    unit_vector
+
+    sent_rank = {}
+    sent_id = 0
+
+    for sent_start, sent_end, sent_vector in sent_bounds:
+        print(sent_vector)
+        sum_sq = 0.0
+
+        for phrase_id in range(len(unit_vector)):
+            print(phrase_id, unit_vector[phrase_id])
+
+            if phrase_id not in sent_vector:
+                sum_sq += unit_vector[phrase_id] ** 2.0
+
+        sent_rank[sent_id] = sqrt(sum_sq)
+        sent_id += 1
+
+    print(sent_rank)
+
+    from operator import itemgetter
+
+    sorted(sent_rank.items(), key=itemgetter(1))
+
+    limit_sentences = 20
+
+    sent_text = {}
+    sent_id = 0
+
+    for sent in doc.sents:
+        sent_text[sent_id] = sent.text
+        sent_id += 1
+
+    num_sent = 0
+
+    for sent_id, rank in sorted(sent_rank.items(), key=itemgetter(1)):
+        print(sent_id, sent_text[sent_id])
+        num_sent += 1
+
+        if num_sent == limit_sentences:
+            break
 
 
-run_nlp(text)
+run_nlp()
