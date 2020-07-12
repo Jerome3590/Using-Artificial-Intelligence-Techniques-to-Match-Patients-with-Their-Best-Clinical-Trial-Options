8416e2286b6616b63e028a55d0cbdb4293d07a1c
Jerome Dixon <jerome.dixon90@gmail.com>
2020-07-12 13:35:36 -0400

    Update NLP_Processor.py

--- NLP_Processor.py
+++ NLP_Processor.py
@@ -8,12 +8,12 @@ import operator
 
 nlp = spacy.load("en_core_web_sm")
 
-text = "Compatibility of systems of linear constraints over the set of natural numbers. Criteria of compatibility of " \
-       "a system of linear Diophantine equations, strict inequations, and nonstrict inequations are considered. Upper " \
-       "bounds for components of a minimal set of solutions and algorithms of construction of minimal generating sets " \ 
-       "of solutions for all types of systems are given. These criteria and the corresponding algorithms for " \
-       "constructing a minimal supporting set of solutions can be used in solving all the considered types systems " \
-       "and systems of mixed types. "
+text = """Compatibility of systems of linear constraints over the set of natural numbers. Criteria of compatibility of
+a system of linear Diophantine equations, strict inequations, and nonstrict inequations are considered. Upper
+bounds for components of a minimal set of solutions and algorithms of construction of minimal generating sets
+of solutions for all types of systems are given. These criteria and the corresponding algorithms for
+constructing a minimal supporting set of solutions can be used in solving all the considered types systems
+and systems of mixed types."""
 
 doc = nlp(text)
 
